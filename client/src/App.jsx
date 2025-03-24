cbaa4333ccfc8634109a62bc7bbccb4a6cf756a0
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2025-03-24 21:26:13 +0530

    Authentication Start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
-import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AdminLayout } from "./components/admin-view/layout";
 import { AuthLayout } from "./components/auth/layout";
+import { CheckAuth } from "./components/common/check-auth";
 import { ShoppingLayout } from "./components/shopping-view/layout";
 import { AdminDashboard } from "./pages/admin-view/dashboard";
 import { AdminFeatures } from "./pages/admin-view/features";
@@ -12,17 +12,31 @@ import { AuthRegister } from "./pages/auth/register";
 import { NotFound } from "./pages/not-found";
 
 export const App = () => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  console.log(isAuthenticated);
+  const isAuthenticated = false;
+  const user = null;
   return (
     <>
       <div className="flex flex-column overflow-hidden bg-white">
         <Routes>
-          <Route path="/auth" element={<AuthLayout />}>
+          <Route
+            path="/auth"
+            element={
+              <CheckAuth isAuthenticated={{ isAuthenticated }} user={user}>
+                <AuthLayout />
+              </CheckAuth>
+            }
+          >
             <Route path="login" element={<AuthLogin />} />
             <Route path="register" element={<AuthRegister />} />
           </Route>
-          <Route path="/admin" element={<AdminLayout />}>
+          <Route
+            path="/admin"
+            element={
+              <CheckAuth isAuthenticated={{ isAuthenticated }} user={user}>
+                <AdminLayout />
+              </CheckAuth>
+            }
+          >
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="products" element={<AdminProducts />} />
             <Route path="orders" element={<AdminOrders />} />
