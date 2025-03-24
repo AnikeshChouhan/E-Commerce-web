60b7762a76262319dc1e8d6d9229563cfba9e5f0
Anikesh Chouhan <anikeshmaheshwari558@gmail.com>
2025-03-24 19:34:15 +0530

    Basic structure of Shopping page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,10 @@ import { AdminProducts } from "./pages/admin-view/products";
 import { AuthLogin } from "./pages/auth/login";
 import { AuthRegister } from "./pages/auth/register";
 import { NotFound } from "./pages/not-found";
+import { ShoppingAccount } from "./pages/shopping-view/account";
+import { ShoppingCheckout } from "./pages/shopping-view/checkout";
+import { ShoppingHome } from "./pages/shopping-view/home";
+import { ShoppingListing } from "./pages/shopping-view/listing";
 
 export const App = () => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -18,19 +22,26 @@ export const App = () => {
     <>
       <div className="flex flex-column overflow-hidden bg-white">
         <Routes>
+          {/* auth */}
           <Route path="/auth" element={<AuthLayout />}>
             <Route path="login" element={<AuthLogin />} />
             <Route path="register" element={<AuthRegister />} />
           </Route>
+          {/* admin */}
           <Route path="/admin" element={<AdminLayout />}>
             <Route path="dashboard" element={<AdminDashboard />} />
             <Route path="products" element={<AdminProducts />} />
             <Route path="orders" element={<AdminOrders />} />
             <Route path="features" element={<AdminFeatures />} />
           </Route>
+          {/* shopping */}
           <Route path="/shop" element={<ShoppingLayout />}>
-            <Route path="" element={<NotFound />} />
+            <Route path="home" element={<ShoppingHome />} />
+            <Route path="list" element={<ShoppingListing />} />
+            <Route path="account" element={<ShoppingAccount />} />
+            <Route path="checkout" element={<ShoppingCheckout />} />
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </>
---
+++ client/src/pages/shopping-view/home.jsx
@@ -0,0 +1,3 @@
+export const ShoppingHome = () => {
+  return <div>ShoppingHome</div>;
+};
